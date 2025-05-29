0a821a42ede4727d8b94c96fd4b3b36b372e61b5
mehta-asim <asimmehtaidp@gmail.com>
2025-05-29 18:12:31 -0400

    added top 10 words used in the book

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_number_of_words, get_number_of_characters, get_sorted_characters
+from stats import get_number_of_words, get_number_of_characters, get_sorted_characters, get_words_with_numbers
 import sys
 
 def get_book_text(filepath): 
@@ -15,11 +15,18 @@ def main():
     number_of_words = get_number_of_words(book_text)
     number_of_characters = get_number_of_characters(book_text)
     sorted_characters = get_sorted_characters(number_of_characters)
+    words = get_words_with_numbers(book_text)
+    sorted_words = get_sorted_characters(words)
+    top_10_words = list(sorted_words.items())[:10]
+
 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
     print(f"Found {number_of_words} total words")
+    print("----------- Top 10 Words ----------")
+    for word, value in top_10_words:
+        print(f"{word}: {value}")
     print("--------- Character Count -------")
     for character in sorted_characters:
         if character.isalpha() == True:
--- stats.py
+++ stats.py
@@ -1,7 +1,19 @@
 def  get_number_of_words(book_text):
     number_of_words = len(book_text.split())
     return number_of_words
+
+def get_words_with_numbers(book_text):
+    word_with_numbers = {}
+    words = book_text.split()
+    for word in words:
+        word = word.lower()
+        if word in word_with_numbers:
+            word_with_numbers[word] += 1
+        else:
+            word_with_numbers[word] = 1
     
+    return word_with_numbers
+
 
 def get_number_of_characters(book_text):
     characters = {}
